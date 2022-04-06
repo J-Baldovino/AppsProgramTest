8a10861bce6e68ae01cc101a20ea0f0e9883dad1
martinsanabia <99694399+martinsanabia@users.noreply.github.com>
2022-04-06 16:50:30 -0500

    Add files via upload

---
+++ AppsProject/src/application/model/Champions.java
@@ -0,0 +1,33 @@
+package application.model;
+
+public class Champions {
+	
+	private String name;
+	private String score;
+	
+	public Champions(String name, String score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+	
+	
+	
+	
+
+}
