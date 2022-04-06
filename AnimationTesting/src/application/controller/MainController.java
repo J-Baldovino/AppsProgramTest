7d39a18a71a69e7329089ccae0518083c3bc4c3f
J-Baldovino <81478968+J-Baldovino@users.noreply.github.com>
2022-04-05 23:54:56 -0500

    Animation with buttons to start and pause

--- AnimationTesting/src/application/controller/MainController.java
+++ AnimationTesting/src/application/controller/MainController.java
@@ -1,5 +1,6 @@
 package application.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -8,8 +9,10 @@ import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
@@ -19,10 +22,36 @@ public class MainController implements Initializable{
 	@FXML
 	private ImageView pika, sanic, monke, ant;
 	
+	@FXML
+	private Button startButton, pauseButton;
+	
+	private TranslateTransition translatePika = new TranslateTransition();
+	
+	private RotateTransition rotateSanic = new RotateTransition();
+	
+	private FadeTransition fade = new FadeTransition();
+	
+	private ScaleTransition scale = new ScaleTransition();
+	
+	@FXML
+    void startMove(ActionEvent event) throws IOException{
+		translatePika.play();
+		rotateSanic.play();
+		fade.play();
+		scale.play();
+    }
+
+    @FXML
+    void pauseMove(ActionEvent event) {
+    	translatePika.pause();
+    	rotateSanic.pause();
+    	fade.pause();
+    	scale.pause();
+    }
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		//Translate (move) the pika image
-		TranslateTransition translatePika = new TranslateTransition();
 		translatePika.setNode(pika);
 		translatePika.setDuration(Duration.millis(1000)); //sets the duration of the entire animation, so in this instance it would take 1000 milliseconds or a second
 		//translate.setCycleCount(2); //repeats the animation two times
@@ -31,10 +60,8 @@ public class MainController implements Initializable{
 		translatePika.setByX(500); //moves the image to the right by 500 pixels
 		translatePika.setByY(-200); //moves the image up by 200 pixels
 		translatePika.setAutoReverse(true); //causes the image to reverse automatically at the end of our translation
-		translatePika.play();
 		
 		//Rotate the sanic image
-		RotateTransition rotateSanic = new RotateTransition();
 		rotateSanic.setNode(sanic);
 		rotateSanic.setDuration(Duration.millis(1000)); //sets the duration of the entire animation, so in this instance it would take 1000 milliseconds or a second
 		//translate.setCycleCount(2); //repeats the animation two times
@@ -43,10 +70,8 @@ public class MainController implements Initializable{
 		rotateSanic.setInterpolator(Interpolator.LINEAR);
 		rotateSanic.setAxis(Rotate.Z_AXIS); //Rotate. also has the X_AXIS and Y_AXIS options
 		rotateSanic.setByAngle(360);
-		rotateSanic.play();
 		
 		//Fade away the monke image
-		FadeTransition fade = new FadeTransition();
 		fade.setNode(monke);
 		fade.setDuration(Duration.millis(2000)); //sets the duration of the entire animation, so in this instance it would take 1000 milliseconds or a second
 		//translate.setCycleCount(2); //repeats the animation two times
@@ -55,10 +80,8 @@ public class MainController implements Initializable{
 		//If you want an image to fade in, you can set the two following as setFromValue(0) and setToValue(1)
 		fade.setFromValue(1); //original opacity value
 		fade.setToValue(0);	//target opacity value
-		fade.play();
 		
 		//Scale the antman image
-		ScaleTransition scale = new ScaleTransition();
 		scale.setNode(ant);
 		scale.setDuration(Duration.millis(2000)); //sets the duration of the entire animation, so in this instance it would take 1000 milliseconds or a second
 		//translate.setCycleCount(2); //repeats the animation two times
@@ -67,7 +90,6 @@ public class MainController implements Initializable{
 		scale.setByX(2.0); //Stretches to twice the size of the original image on the X axis
 		scale.setByY(2.0); //Stretches to twice the size of the original image on the Y axis
 		scale.setAutoReverse(true);
-		scale.play();
 	}
 
 }
