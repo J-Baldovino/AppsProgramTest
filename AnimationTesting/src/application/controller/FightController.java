c229b25100c7dfc52fc358359b68dc5e76ea812f
J-Baldovino <81478968+J-Baldovino@users.noreply.github.com>
2022-04-14 22:18:15 -0500

    Added small animation details and also a healing button with effect

--- AnimationTesting/src/application/controller/FightController.java
+++ AnimationTesting/src/application/controller/FightController.java
@@ -3,6 +3,7 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
@@ -16,27 +17,41 @@ import javafx.util.Duration;
 
 public class FightController implements Initializable{
     @FXML
-    private Button attackButton;
+    private Button attackButton, healButton;
 
     @FXML
-    private ImageView sanic, goomba, sword;
+    private ImageView sanic, goomba, sword, heal;
     
     private TranslateTransition translateSword = new TranslateTransition();
     private RotateTransition rotateSword = new RotateTransition();
     private TranslateTransition translateGoomba = new TranslateTransition();
     private TranslateTransition translateSanic = new TranslateTransition();
+    private FadeTransition fadeGoomba = new FadeTransition();
+    private FadeTransition fadeHeal = new FadeTransition();
 
     @FXML
     void attack(ActionEvent event) {
-//    	rotateSword.play();
+    	sword.setVisible(true);
+    	heal.setVisible(false);
     	translateSword.play();
     	rotateSword.play();
     	translateGoomba.play();
     	translateSanic.play();
+    	fadeGoomba.play();
+    }
+    
+    @FXML
+    void healing(ActionEvent event) {
+    	sword.setVisible(false);
+    	heal.setVisible(true);
+    	fadeHeal.play();
+    	
     }
     
     @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+    	sword.setVisible(false); //start the sword as not visible
+    	heal.setVisible(false);
     	//Preparing rotation movement for sword
     	rotateSword.setNode(sword);
     	rotateSword.setDuration(Duration.millis(100));
@@ -66,6 +81,23 @@ public class FightController implements Initializable{
 		translateSanic.setCycleCount(2);
 		translateSanic.setByY(-35); 
 		translateSanic.setAutoReverse(true);
+		
+		//Preparing fade animation for the enemy
+		fadeGoomba.setNode(goomba);
+		fadeGoomba.setDuration(Duration.millis(1000));
+		fadeGoomba.setCycleCount(1);
+		fadeGoomba.setInterpolator(Interpolator.EASE_OUT); //Causes the animation to slow down near the end of the sequence
+		fadeGoomba.setFromValue(1); //original opacity value
+		fadeGoomba.setToValue(0);	//target opacity value
+		
+		//Preparing fade animation for healing
+		fadeHeal.setNode(heal);
+		fadeHeal.setDuration(Duration.millis(1000));
+		fadeHeal.setCycleCount(2);
+		fadeHeal.setInterpolator(Interpolator.EASE_OUT); //Causes the animation to slow down near the end of the sequence
+		fadeHeal.setAutoReverse(true);
+		fadeHeal.setFromValue(0); //original opacity value
+		fadeHeal.setToValue(1);	//target opacity value
     }
 
 }
--- AnimationTesting/src/application/controller/MainController.java
+++ AnimationTesting/src/application/controller/MainController.java
@@ -2,7 +2,6 @@ package application.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
